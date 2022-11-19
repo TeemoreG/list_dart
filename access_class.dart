2a94b926b5317ce3969e5d65e028bf2fd95021c7
TeemoreG <timblax18@gmail.com>
2022-11-19 13:39:49 +0300

    first commit

---
+++ access_class.dart
@@ -0,0 +1,11 @@
+import "class.dart";
+void main(){
+    Student student1=Student();
+    student1.reg_no="P100/1669G/21";
+    student1.name="Timothy njenga";
+    student1.id_no=39874476;
+    student1.adm_no="4060";
+    student1.course="Information  technology";
+    print("student name:${student1");
+
+}
